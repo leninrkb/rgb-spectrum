9401bdebd0047f71a42fb174ca1fa51cda13d198
Lenin Acosta <rkb.lenin@gmail.com>
2024-02-29 23:07:06 -0500

    VRow

---
+++ src/layouts/VRow.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="myclass">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+.myclass {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,27 @@
 <template>
-    <div>
-        <ElevatedCard>
-            holaa
-        </ElevatedCard>
-    </div>
-    <div>
-        2
-    </div>
-    <div>
-        3
-    </div>
+    <ElevatedCard>
+        Seccion 1
+    </ElevatedCard>
+    <ElevatedCard>
+        Seccion 2
+    </ElevatedCard>
+    <ElevatedCard>
+        Seccion 3
+        <br>
+        <VRow>
+            <div class="bg-teal-500">
+                Lenin (TurtleBytes)
+            </div>
+            <div class="bg-teal-500">
+                Sana Sunomiya
+            </div>
+        </VRow>
+    </ElevatedCard>
 </template>
 
 <script setup>
-import ElevatedCard from '../components/ElevatedCard.vue'
+import ElevatedCard from '../components/ElevatedCard.vue';
+import VRow from '../layouts/VRow.vue';
 </script>
+
+<style scoped></style>
