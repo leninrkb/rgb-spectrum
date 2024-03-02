12d573d6b025b99b66d270636bc5296364fe5b53
Lenin Acosta <rkb.lenin@gmail.com>
2024-03-02 17:41:46 -0500

    go back function

--- src/views/BrowseView.vue
+++ src/views/BrowseView.vue
@@ -1,7 +1,7 @@
 <template>
     <VColumn>
         <div>
-            <IconButton :source="arrow_back"></IconButton>
+            <IconButton :source="arrow_back" @click="go_back"></IconButton>
         </div>
         <div>
             googly
@@ -16,9 +16,16 @@
 </template>
 
 <script setup>
-import IconButton from '@/components/IconButton.vue'
+import IconButton from '@/components/IconButton.vue';
 import VColumn from '@/layouts/VColumn.vue';
-let arrow_back = require('../assets/arrow_back_FILL1_wght400_GRAD0_opsz24.svg')
+import { useRouter } from 'vue-router';
+
+let arrow_back = require('../assets/arrow_back_FILL1_wght400_GRAD0_opsz24.svg');
+const router = useRouter();
+
+const go_back = function () {
+    router.go(-1);
+}
 </script>
 
 <style scoped></style>
