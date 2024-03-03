8d5bb299903d6751502fddd8c62c5b98d8c52000
Lenin Acosta <rkb.lenin@gmail.com>
2024-03-03 17:55:48 -0500

    create textfield

---
+++ src/components/TextField.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <input type="text" v-on:release="emit_data" v-model="input_data">
+        <img src="" alt="icon">
+        <img src="" alt="icon">
+    </div>
+</template>
+
+<script setup>
+import { defineEmits, ref } from 'vue'
+const emit = defineEmits(['data'])
+let input_data = ref('')
+const emit_data = () => {
+    emit('data', input_data)
+}
+</script>
+
+<style scoped></style>
