25e46dad7f949ab1637e72db038bbfcf1598b8fd
Lenin Acosta <rkb.lenin@gmail.com>
2024-03-03 15:49:05 -0500

    googly logo

--- src/views/BrowseView.vue
+++ src/views/BrowseView.vue
@@ -4,7 +4,16 @@
             <GoBack></GoBack>
         </div>
         <div>
-            googly
+            <VRow>
+                <div class="logo">
+                    <span>G</span>
+                    <span>o</span>
+                    <span>o</span>
+                    <span>g</span>
+                    <span>l</span>
+                    <span>y</span>
+                </div>
+            </VRow>
         </div>
         <div>
             search field
@@ -18,7 +27,38 @@
 <script setup>
 import VColumn from '@/layouts/VColumn.vue';
 import GoBack from '@/components/GoBack.vue';
+import VRow from '@/layouts/VRow.vue';
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.logo {
+    font-size: 5em;
+    font-family: sans-serif;
+    text-shadow: 1px 1px 4px;
+
+    & span:nth-child(1) {
+        color: rgb(51, 51, 255);
+    }
+
+    & span:nth-child(2) {
+        color: rgb(250, 62, 62);
+    }
+
+    & span:nth-child(3) {
+        color: rgb(250, 187, 62);
+    }
+
+    & span:nth-child(4) {
+        color: rgb(51, 51, 255);
+    }
+
+    & span:nth-child(5) {
+        color: rgb(0, 177, 6);
+    }
+
+    & span:nth-child(6) {
+        color: rgb(250, 62, 62);
+    }
+}
+</style>
