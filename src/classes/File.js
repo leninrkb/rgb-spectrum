0ecb9566fb1990c5243330c442bdb6de084081dc
Lenin Acosta <rkb.lenin@gmail.com>
2024-03-06 03:26:14 -0500

    upload img

---
+++ src/classes/File.js
@@ -0,0 +1,7 @@
+class File {
+  constructor() {
+    this.file = null;
+  }
+
+  read_file(file) {}
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,13 +4,15 @@
             <VRow>
                 <div id="upload_container">
                     <FlatButton>Upload Image</FlatButton>
-                    <input id="upload_input" type="file">
+                    <input id="upload_input" type="file" @change="file_handler">
                 </div>
                 <FlatButton><router-link :to="{ name: 'browse' }">Browse on web</router-link></FlatButton>
             </VRow>
         </ElevatedCard>
         <ElevatedCard>
-            Seccion 2
+            title: selected img
+            img: <img :src="img" alt="selected img">
+            info: m * n
         </ElevatedCard>
         <ElevatedCard>
             seccion 3
@@ -23,6 +25,15 @@ import ElevatedCard from '../components/ElevatedCard.vue';
 import VRow from '../layouts/VRow.vue';
 import VColumn from '../layouts/VColumn.vue'
 import FlatButton from '../components/FlatButton.vue';
+import { ref } from "vue";
+
+let img = ref(null);
+
+const file_handler = (e) => {
+    const file = e.target.files[0];
+    let url_file = URL.createObjectURL(file);
+    img.value = url_file;
+}
 </script>
 
 <style scoped lang="scss">
