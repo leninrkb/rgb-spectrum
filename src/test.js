0612fffb65ec0e482019ef39e9083d50ec7cc427
Lenin Acosta <rkb.lenin@gmail.com>
2024-03-06 03:58:04 -0500

    alegra solution and file class

--- src/test.js
+++ src/test.js
@@ -1,15 +1,39 @@
-// funcion de orden superior
+const good_array = (myarray) => {
+  let newarray = [myarray[0]];
+  let n = 0;
+  let m = 0;
 
-function higher(callback) {
-    return callback
-}
+  while (n < myarray.length) {
+    let current = myarray[n];
+    if (current !== newarray[m]) {
+      newarray.push(current);
+      m += 1;
+    }
+    n += 1;
+  }
 
-higher();
+  return newarray.join("");
+};
 
-// funciones de este tipo: map - filter - reduce
+let resp = good_array([
+  "a",
+  "a",
+  "a",
+  "a",
+  "a",
+  "a",
+  "a",
+  "a",
+  "a",
+  "b",
+  "b",
+  "b",
+  "b",
+  "b",
+  "a",
+  "a",
+  "c",
+  "b",
+]);
 
-const pila = [1, 23, 4, 5]
-pila.map(function () {
-
-})
-
+console.log(resp);
